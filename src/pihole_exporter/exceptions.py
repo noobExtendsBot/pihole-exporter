094cdfc3ec8da41fea8e300a4e094aeacecc430b
noobExtendsBot <hackerearth02@gmail.com>
2026-05-30 11:22:56 +0100

    fix mypy for bad auth

--- src/pihole_exporter/exceptions.py
+++ src/pihole_exporter/exceptions.py
@@ -1,4 +1,4 @@
 class BadAuthError(Exception):
-    def __init__(self, message="Authentication failed, check your password.", status_code=401):
+    def __init__(self, message: str = "Authentication failed, check your password.", status_code: int = 401) -> None:
         self.status_code = 401
         super().__init__(message)
