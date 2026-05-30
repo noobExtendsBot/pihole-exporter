442947a8edfaaac9c14f5a5e9f2661926164cfeb
noobExtendsBot <hackerearth02@gmail.com>
2026-05-30 11:42:42 +0100

    improve type annotation and add a new exception

--- src/pihole_exporter/exceptions.py
+++ src/pihole_exporter/exceptions.py
@@ -2,3 +2,9 @@ class BadAuthError(Exception):
     def __init__(self, message: str = "Authentication failed, check your password.", status_code: int = 401) -> None:
         self.status_code = 401
         super().__init__(message)
+
+
+class PiholeRequestError(Exception):
+    def __init__(self, message: str, cause: Exception) -> None:
+        self.cause = cause
+        super().__init__(message)
